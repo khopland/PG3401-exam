c57e68819d451c07e77f6f86a0d03fd461a5db70
Kristoffer Larsen Hopland <24739853+BM0B0T@users.noreply.github.com>
2022-12-08 17:41:37 +0100

    oppagve 2 done

--- oppgave_2/main.c
+++ oppgave_2/main.c
@@ -1,12 +1,35 @@
-#include <stdio.h>
 #include "debugger.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
-{
-    debug("test %d", 1);
-    debug("test %d", 2);
-    debug("test %d", 2);
-    debug("test %d", 2);
-    error("error test");
+int main(void) {
+    FILE *fTarget = NULL; // file for reading
+    FILE *fResult = NULL; // file for writing
+    char c; 
+    char *targetFile = "data.txt"; // name of target file
+    char *ResultFile = "data_result.txt";  // name of result file
+    
+    fTarget = fopen(targetFile, "r"); //  opens read file stream.
+    if (fTarget == NULL) {
+        error("failed to create fTarget");
+        return 1;
+    }
+    fResult = fopen(ResultFile, "w"); //  opens write file stream.
+    if (fResult == NULL) {
+        error("failed to create fResult");
+        return 1;
+    }
+    // read each char and print it as hex
+    do {
+        c = fgetc(fTarget);
+        fprintf(fResult, "%02X", c);
+    } while (!feof(fTarget));
+
+    // clean up / close file readers.
+    fclose(fTarget);
+    fTarget = NULL;
+    fclose(fResult);
+    fResult = NULL;
     return 0;
 }
